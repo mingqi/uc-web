febe6c0fc302d00e36ac3972535cd8d49e3f260f
blogbbs <blogbbs@gmail.com>
2014-10-02 23:10:30 +0800

    1.分离js文件，2.用grunt压缩js文件

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,35 @@ module.exports = function(grunt) {
       }
     },
 
+    coffee: {
+      options: {
+        bare: true
+      },
+      compile: {
+        files: {
+          'public/s/console/log_pattern.js': 'public/s/console/uncompressed/log_pattern.coffee',
+        }
+      },
+    },
+
+    uglify: {
+      options: {
+        sourceMap: true,
+        sourceMapIncludeSources: true,
+      },
+      compile: {
+        files: {
+          'public/s/console/search.min.js': [
+            'public/s/console/log_pattern.js',
+            'public/s/console/uncompressed/search.js',
+          ],
+          'public/s/console/config.min.js': [
+            'public/s/console/uncompressed/config.js',
+          ]
+        }
+      }
+    },
+
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
@@ -29,7 +58,10 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build', 'watch']);
+  grunt.registerTask('css', ['sass']);
+  grunt.registerTask('js', ['coffee', 'uglify']);
+  grunt.registerTask('default', ['css', 'js', 'watch']);
 };
